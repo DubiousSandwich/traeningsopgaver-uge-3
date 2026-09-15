7238c62b3fddd64f33887913538f9b5a189da726
Naya Schwartz <schwartznaya@gmail.com>
2026-09-15 10:42:47 +0200

    more

--- src/Level2.java
+++ src/Level2.java
@@ -2,7 +2,7 @@ public class Level2 {
 
     void main(){
         // DEL A
-
+        /*
         //opgave 2.3
         greet("Naya");
         greet("Sebastian");
@@ -22,6 +22,22 @@ public class Level2 {
         //2.17
         printFullName("Naya", "Schwartz");
 
+        //2.23
+        printCoordinates(4,2);
+
+        //DEL D
+        //2.25,26
+        printIfPositive(7);
+
+        //2.31
+        printLarger(5,5);
+
+        //DEL E
+        //2.34
+
+
+    */
+
     }
 
     void greet(String name){
@@ -37,5 +53,28 @@ public class Level2 {
     void printFullName(String firstName, String lastName){
         System.out.println(firstName + " " + lastName);
     }
+
+    void printCoordinates(int x, int y){ System.out.println("(" + x + "," + y + ")");}
+
+    void printIfPositive(int number){
+        if (number % 2 ==0 && number > 0){
+            System.out.println(number);
+        }
+    }
+
+    void printLarger(int a, int b){
+        if (a > b){
+            System.out.println(a);
+        } else if (a < b){
+            System.out.println(b);
+        } else if (a == b){
+            System.out.println("they are equal");
+        }
+    }
+
+    void printChar(char c, int count){
+
+    }
+
 
 }
